baf9d7a08933a705f6dad237a06f2383bf9de3ee
Julie Nikitina <jq.nikitina@gmail.com>
2021-02-17 00:06:25 +0800

    revert shit

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -80,7 +80,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': (
             'django.contrib.auth.password_validation.'
             'MinimumLengthValidator'
-        ),
+                 ),
     },
     {
         'NAME': (
@@ -124,9 +124,9 @@ REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': [
         'django_filters.rest_framework.DjangoFilterBackend'
     ],
-    'DEFAULT_PAGINATION_CLASS': [
+    'DEFAULT_PAGINATION_CLASS': (
         'rest_framework.pagination.PageNumberPagination'
-    ],
+    ),
     'PAGE_SIZE': 5,
 }
 
